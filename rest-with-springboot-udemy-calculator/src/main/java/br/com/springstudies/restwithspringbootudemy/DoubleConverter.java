e42a469920e5baaf7305c9cd0d2902ca13e764a9
Guilherme <guilhermemirar@gmail.com>
2021-03-24 20:04:47 -0300

    REST calculator

---
+++ rest-with-springboot-udemy-calculator/src/main/java/br/com/springstudies/restwithspringbootudemy/DoubleConverter.java
@@ -0,0 +1,2 @@
+package br.com.springstudies.restwithspringbootudemy;public class DoubleConverter {
+}
---
+++ rest-with-springboot-udemy-calculator/src/main/java/br/com/springstudies/restwithspringbootudemy/exception/UnsupportedMathOperationException.java
@@ -0,0 +1,2 @@
+package br.com.springstudies.restwithspringbootudemy.exception;public class UnsupportedMathOperationException {
+}
